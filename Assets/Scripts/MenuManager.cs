a6605a0dd2c23876063b9c69c9c38de4dd30445a
Ashley Yang <ashyang586@gmail.com>
2023-06-02 13:28:42 -0400

    Main and pause menu

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -6,11 +6,6 @@ using UnityEngine.EventSystems;
 
 public class MenuManager : MonoBehaviour
 {
-    public GameObject htpClose, creditClose;
-
-    public GameObject htpMenu;
-    public GameObject creditMenu;
-
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -19,27 +14,5 @@ public class MenuManager : MonoBehaviour
     public void EndGame()
     {
         Application.Quit();
-    }
-
-    public void HowToPlayClose()
-    {
-        //sets the object to true
-        htpMenu.SetActive(false);
-
-        //clears selected object
-        EventSystem.current.SetSelectedGameObject(null);
-        //set a new selected object
-        EventSystem.current.SetSelectedGameObject(htpClose);
-    }
-
-    public void CreditClose()
-    {
-        //sets the object to true
-        creditMenu.SetActive(false);
-
-        //clears selected object
-        EventSystem.current.SetSelectedGameObject(null);
-        //set a new selected object
-        EventSystem.current.SetSelectedGameObject(creditClose);
     }
 }

531eddf7e7e75973873c039347d4b436897579a1
DG526 <dg526@njit.edu>
2023-06-12 14:25:17 -0400

    Wyrms can move and track & damage player.

---
+++ Assets/Enemies/Wyrm Components/WyrmHeadBehavior.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro.Examples;
+using UnityEngine;
+
+public class WyrmHeadBehavior : MonoBehaviour
+{
+    public GameObject segTemplate, endTemplate;
+    public int tailLength;
+    public GameObject[] segments;
+
+    public GameObject player;
+    public float maxSegAngle = 40;//Max angle between segments
+    public float speed = 1.4f;
+
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Find player
+        if (!player)
+            player = GameObject.Find("Player");
+
+        //Find rb
+        rb = GetComponent<Rigidbody2D>();
+
+        //Create segments
+        segments = new GameObject[tailLength];
+        float dist = 0.4f;
+        segments[0] = Instantiate(segTemplate, transform.position + new Vector3(0, -dist, 0.001f), new Quaternion());
+        for (int i = 1; i < tailLength-1; i++)
+        {
+            segments[i] = Instantiate(segTemplate, transform.position + new Vector3(0, -dist, 0.001f * (i + 1)), new Quaternion());
+            dist += 0.4f * transform.localScale.y;
+        }
+        segments[tailLength-1] = Instantiate(endTemplate, transform.position + new Vector3(0, -dist, 0.001f * tailLength), new Quaternion());
+
+        Physics2D.IgnoreCollision(GetComponent<Collider2D>(), segments[0].GetComponent<Collider2D>());
+        //Initialize segments
+        for (int i = 0; i < tailLength;i++)
+        {
+            segments[i].GetComponent<WyrmSegmentBehavior>().head = gameObject;
+            if (i == 0)
+                segments[i].GetComponent<WyrmSegmentBehavior>().following = gameObject;
+            else
+            {
+                segments[i].GetComponent<WyrmSegmentBehavior>().following = segments[i - 1];
+                Physics2D.IgnoreCollision(segments[i - 1].GetComponent<Collider2D>(), segments[i].GetComponent<Collider2D>());
+            }
+            if (i < tailLength - 1)
+                segments[i].GetComponent<WyrmSegmentBehavior>().nextSeg = segments[i + 1];
+            segments[i].GetComponent<SpriteRenderer>().color = GetComponent<SpriteRenderer>().color;
+            segments[i].transform.localScale = transform.localScale;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        if(Vector2.Distance(new Vector2(player.transform.position.x, player.transform.position.y), new Vector2(transform.position.x, transform.position.y)) > 0.5f)
+            Slither();
+    }
+
+    void Slither()
+    {
+        transform.up = new Vector2(player.transform.position.x - transform.position.x, player.transform.position.y - transform.position.y);
+        transform.up.Normalize();
+        rb.MovePosition(rb.position + (Vector2)transform.up * speed * Time.fixedDeltaTime);
+
+        segments[0].GetComponent<WyrmSegmentBehavior>().Slither();
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Player") && !collision.gameObject.GetComponent<PlayerHealth>().grace)
+        {
+            collision.gameObject.GetComponent<PlayerHealth>().Damage(1);
+        }
+    }private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Player") && !collision.gameObject.GetComponent<PlayerHealth>().grace)
+        {
+            collision.gameObject.GetComponent<PlayerHealth>().Damage(1);
+        }
+    }
+}
---
+++ Assets/Enemies/Wyrm Components/WyrmSegmentBehavior.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WyrmSegmentBehavior : MonoBehaviour
+{
+    public GameObject head, following, nextSeg;
+    Rigidbody2D rb;
+    WyrmHeadBehavior hBehav;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        hBehav = head.GetComponent<WyrmHeadBehavior>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Slither()
+    {
+        transform.up = new Vector2(following.transform.position.x - transform.position.x, following.transform.position.y - transform.position.y);
+        transform.up.Normalize();
+        rb.MovePosition(following.transform.position - transform.up * transform.localScale.y * 0.4f);
+
+        if (nextSeg)
+            nextSeg.GetComponent<WyrmSegmentBehavior>().Slither();
+    }
+}

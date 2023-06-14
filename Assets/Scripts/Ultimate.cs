4053afd30c9a2cea14a72a086677fc89e5a5ee19
Chris-Nicodemus <cn7@njit.edu>
2023-06-14 17:03:15 -0400

    Implemented beam ultimate attack.

--- Assets/Scripts/Ultimate.cs
+++ Assets/Scripts/Ultimate.cs
@@ -16,7 +16,7 @@ public class Ultimate : MonoBehaviour
     public PlayerHealth playerHealth;
 
     //AoE cooldown
-    public float aoeCDI = 20;
+    public float aoeCDI = 20f;
     public float aoeCD = 0f;
 
     //End radius of AoE
@@ -28,22 +28,52 @@ public class Ultimate : MonoBehaviour
     //AoE explosion speed (lower the faster)
     public float aoeTime = 1f;
 
-    GameObject AoE;
-    //bool aoeActive = false;
 
+    public float beamCDI = 30f;
+    public float beamCD = 0f;
+    public float beamTickSpeed = 0.1f;
+
+
+    public Vector2 beamSize = Vector2.zero;
+    public Vector2 beamStart = new Vector2(0.1f, 1f);
+    public float beamChargeTime = 1f;
+    public float beamDuration = 8f;
+
+    //script specific
+    GameObject AoE;
+
+    GameObject Beam;
+    bool beamActive;
+    float beamEnd = 0f;
     public string ultimate = "aoe";
+
+    Color beamColor;
+    float beamAlpha;
+    Vector2 beamStartPos;
+    float beamTick = 0f;
+
+    //BoxCollider2D beamCollider;
     void Start()
     {
         playerHealth = gameObject.GetComponent<PlayerHealth>();
 
-        foreach(Transform child in gameObject.transform)
+        foreach (Transform child in gameObject.transform)
         {
-            if(child.gameObject.name != "AoE Ult")
+            if (child.gameObject.name == "AoE Ult")
             {
-                continue;
+                AoE = child.gameObject;
             }
 
-            AoE = child.gameObject;
+            if (child.gameObject.name == "Beam Ult")
+            {
+                Beam = child.gameObject;
+                beamColor = Beam.GetComponent<SpriteRenderer>().color;
+                beamAlpha = beamColor.a;
+
+                beamStartPos = Beam.transform.localPosition;
+
+                //beamCollider = Beam.GetComponent<BoxCollider2D>();
+            }
         }
     }
 
@@ -57,28 +87,62 @@ public class Ultimate : MonoBehaviour
                 FireUltimate(ultimate);
             }
 
+            if (beamActive && Time.time > beamEnd)
+            {
+                //reset color
+                Beam.GetComponent<SpriteRenderer>().color = beamColor;
+                Debug.Log("Beam End");
+                beamActive = false;
+
+                //reset start position and size
+                Beam.transform.localPosition = beamStartPos;
+                Beam.transform.localScale = beamStart;
+
+                //Turn off object
+                Beam.SetActive(false);
+            }
+            else if (beamActive && Time.time > beamTick)
+            {
+                Beam.GetComponent<BoxCollider2D>().enabled = true;
+                //Beam.GetComponent<BoxCollider2D>().isTrigger = true;
+
+                beamTick = Time.time + beamTickSpeed;
+
+                StartCoroutine(BeamTickDamage(0.1f));
+            }
         }
     }
 
-    void FireUltimate (string ult)
+    void FireUltimate(string ult)
     {
-        if(ult == "aoe" && Time.time > aoeCD)
+        if (ult == "aoe" && Time.time > aoeCD)
         {
             aoeCD = Time.time + aoeCDI;
 
-            if(!AoE.activeInHierarchy)
+            if (!AoE.activeInHierarchy)
             {
                 AoE.SetActive(true);
             }
             StartCoroutine(ScaleOverTime(aoeTime));
         }
 
-        if(ult == "clone" && Time.time > cloneCD)
+        if (ult == "clone" && Time.time > cloneCD)
         {
             cloneCD = Time.time + cloneCDI;
 
             GameObject spawnClone = Instantiate(clone, gameObject.transform.position + cloneSpawnPos, gameObject.transform.rotation);
             Destroy(spawnClone, cloneDuration);
+        }
+
+        if (ult == "beam" && Time.time > beamCD)
+        {
+            beamCD = Time.time + beamCDI;
+
+            if (!Beam.activeInHierarchy)
+            {
+                Beam.SetActive(true);
+            }
+            StartCoroutine(BeamOverTime(beamChargeTime));
         }
     }
 
@@ -86,7 +150,7 @@ public class Ultimate : MonoBehaviour
     IEnumerator ScaleOverTime(float time)
     {
         Vector2 originalScale = new Vector2(aoeStart, aoeStart);
-        Vector2 destinationScale = new Vector2 (aoeSize,aoeSize);
+        Vector2 destinationScale = new Vector2(aoeSize, aoeSize);
         AoE.transform.localScale = originalScale;
 
         float currentTime = 0f;
@@ -100,5 +164,46 @@ public class Ultimate : MonoBehaviour
 
         AoE.transform.localScale = originalScale;
         AoE.SetActive(false);
+    }
+
+    IEnumerator BeamOverTime(float time)
+    {
+
+        Color beamColor = Beam.GetComponent<SpriteRenderer>().color;
+        float beamAlpha = beamColor.a;
+        //Color target = new Color(beamColor.r, beamColor.g, beamColor.b, 1f);
+        float currentTime = 0f;
+
+
+        do
+        {
+            Beam.transform.localScale = Vector2.Lerp(beamStart, beamSize, currentTime / time);
+            Beam.transform.localPosition = Vector2.Lerp(beamStartPos, (new Vector2(0f, beamSize.y) + beamStartPos) / 2, currentTime / time);
+
+            if (beamColor.a > 0.5f && !beamActive)
+            {
+                beamActive = true;
+                beamEnd = Time.time + beamDuration;
+
+                Debug.Log("Beam Active");
+            }
+
+            beamColor.a = Mathf.Lerp(beamAlpha, 1f, currentTime / time);
+            Beam.GetComponent<SpriteRenderer>().color = beamColor;
+            //Debug.Log(beamColor.a);
+            currentTime += Time.deltaTime;
+            yield return null;
+        } while (currentTime < time);
+    }
+
+    IEnumerator BeamTickDamage(float time)
+    {
+        //float currentTime = 0f;
+
+
+        yield return new WaitForSeconds(time);
+
+        //Beam.GetComponent<BoxCollider2D>().isTrigger = false;
+        Beam.GetComponent<BoxCollider2D>().enabled = false;
     }
 }

b6af480313349cb95fb1fb08b44ff5ce3094e904
Ashley Yang <ashyang586@gmail.com>
2023-06-16 13:56:16 -0400

    Primary, secondary, ultimate HUD kinda working

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public int maxHealth = 6;
+    public int maxHealth = 5;
     public int health;
     public bool dead = false;
     public bool grace = false;

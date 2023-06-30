283603f655ad75cb1d1dbd34ac6a807cfb21dfd3
Ashley Yang <ashyang586@gmail.com>
2023-06-30 14:22:43 -0400

    Loadout Complete

--- Assets/UI Stuff/Shopping.cs
+++ Assets/UI Stuff/Shopping.cs
@@ -47,7 +47,6 @@ public class Shopping : MonoBehaviour
         shoot = player.GetComponent<Shooting>();
 
         shopCanvas.SetActive(false);
-        Time.timeScale = 1f;
     }
 
     private void Update()
